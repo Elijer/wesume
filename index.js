0700efa831633fa83f451b1b45409b3607513182
Jah <kennedye2@vcu.edu>
2020-11-22 15:19:09 -0500

    added some print handling

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
 console.log('behavior.js');
 
 window.addEventListener('load', (event) => {
+
     console.log('page is fully loaded');
+    var def = 10;
+    var blue = '#4aa1e0'
+
+    document.onmousemove = function(e){
+        console.log("hey")
+
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        var x = e.x;
+        var y = e.y;
+
+        var xR = x/w;
+        var yR = y/h;
+
+        var els = document.getElementsByClassName("highlight");
+        var els2 = document.getElementsByClassName("highlight2");
+        var name = document.getElementById("name");
+        name.style.textShadow = `${def * xR}px ${def * yR}px #e5deb5`;
+
+        for (var i = 0; i < els.length; i++){
+            els[i].style.textShadow = `${def * xR}px ${def * yR}px #4aa1e0`;
+        }
+        
+        for (var i = 0; i < els2.length; i++){
+            els2[i].style.textShadow = `${def * xR}px ${def * yR}px #cd4f4f`;
+        }
+
+    }
   });
+
+  window.onbeforeprint = function(event) {
+    console.log("yopr");
+    var els = document.getElementsByClassName("highlight");
+    var els2 = document.getElementsByClassName("highlight2");
+    var name = document.getElementById("name");
+
+    name.style.textShadow = 'none';
+
+    for (var i = 0; i < els.length; i++){
+        els[i].style.textShadow = 'none';
+    }
+    
+    for (var i = 0; i < els2.length; i++){
+        els2[i].style.textShadow = 'none';
+    }
+
+  };
